24035b8360437e548df42fca2f6492e18a14af21
S.Mendoza <mendoza.sharmaine022@gmail.com>
2023-06-02 12:57:52 +0800

    modified code admin prodList and orderList

--- frontend/src/pages/ProductListPage.js
+++ frontend/src/pages/ProductListPage.js
@@ -29,17 +29,37 @@ const reducer = (state, action) => {
       return { ...state, loadingCreate: false };
     case 'CREATE_FAIL':
       return { ...state, loadingCreate: false };
+
+    case 'DELETE_REQUEST':
+      return { ...state, loadingDelete: true, successDelete: false };
+    case 'DELETE_SUCCESS':
+      return { ...state, loadingDelete: false, successDelete: true };
+    case 'DELETE_FAIL':
+      return { ...state, loadingDelete: false, successDelete: false };
+    case 'DELETE_RESET':
+      return { ...state, loadingDelete: false, successDelete: false };
+
     default:
       return state;
   }
 };
 
 export default function ProductListPage() {
-  const [{ loading, error, products, pages, loadingCreate }, dispatch] =
-    useReducer(reducer, {
-      loading: true,
-      error: '',
-    });
+  const [
+    {
+      loading,
+      error,
+      products,
+      pages,
+      loadingCreate,
+      loadingDelete,
+      successDelete,
+    },
+    dispatch,
+  ] = useReducer(reducer, {
+    loading: true,
+    error: '',
+  });
 
   const navigate = useNavigate();
   const { search } = useLocation();
@@ -61,8 +81,12 @@ export default function ProductListPage() {
         dispatch({ type: 'FETCH_FAIL', payload: getError(error) });
       }
     };
-    fetchData();
-  }, [page, userInfo]);
+    if (successDelete) {
+      dispatch({ type: 'DELETE_RESET' });
+    } else {
+      fetchData();
+    }
+  }, [page, userInfo, successDelete]);
 
   const createHandler = async () => {
     if (window.confirm('Create new product?')) {
@@ -83,6 +107,21 @@ export default function ProductListPage() {
     }
   };
 
+  const deleteHandler = async (product) => {
+    if (window.confirm('Are you sure to delete?')) {
+      try {
+        await axios.delete(`/api/products/${product._id}`, {
+          headers: { authorization: `Bearer ${userInfo.token}` },
+        });
+        toast.success('Product successfully deleted.');
+        dispatch({ type: 'DELETE_SUCCESS' });
+      } catch (err) {
+        toast.error(getError(err));
+        dispatch({ type: 'DELETE_FAIL' });
+      }
+    }
+  };
+
   return (
     <div>
       <Row>
@@ -99,6 +138,7 @@ export default function ProductListPage() {
       </Row>
 
       {loadingCreate && <LoadingBox />}
+      {loadingDelete && <LoadingBox />}
 
       {loading ? (
         <LoadingBox />
@@ -131,6 +171,13 @@ export default function ProductListPage() {
                       variant="light"
                       onClick={() => navigate(`/admin/product/${product._id}`)}>
                       Edit
+                    </Button>
+                    &nbsp; &nbsp;
+                    <Button
+                      type="button"
+                      variant="light"
+                      onClick={() => deleteHandler(product)}>
+                      Delete
                     </Button>
                   </td>
                 </tr>
